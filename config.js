486394ed96cc7956dfa3df3c436268b300595a2f
JoseTungui <a310996@uach.mx>
2019-11-27 12:29:16 -0700

    Se volvio a cambiar el config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "https://bc154523-0ee0-4-231-b9ee.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "1zTtdcQjKBFj0Kch0x0DUL9bNYeBwWFtjykb1DHBOlYHMIh7pMGkiIsbIOXMbSTAzLUKjdfyGgIrjxjMc8doqA==";
+config.host = process.env.HOST || "https://northwind.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "90xMbUUvgElya0EzuoxXGcgcVFMoEq80RSkLkAC5gd2xJy4RYwYt5FnqI0VXSATzY766wdV0F7EjI1WE8R1vyQ==";
 config.databaseId = "ToDoList";
 config.collectionId = "Territories";
 
