eb07af88461d5519f152883b8b2a1b681fcfa48d
JoseTungui <a310996@uach.mx>
2019-11-26 22:50:29 -0700

    Se agrego nuevo servidor

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "https://65ab9072-0ee0-4-231-b9ee.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "9awikjFXt6nGW1dlgKPH1H1MIagwgo42cJEPtqWZTfaVMtNmp5577zplEjoeQ16KWNGNvjPEa86FvsjFQvPoKA==";
+config.host = process.env.HOST || "https://c42e42ff-0ee0-4-231-b9ee.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "PQggbVmmzJoez3Knbehe005aq97KYsrvzPSjRTa1ji40cU5TjvaZHvRZgITu75OpLal3lKexF2nekywNlI17nA==";
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
