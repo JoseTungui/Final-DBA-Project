f7cfd4e8f66c3190d219e54e04783cbe64a6a9aa
Jose98Padilla <a310901@uach.mx>
2019-11-28 04:03:04 -0700

    Pestaña para colecciones e identificacion base de datos

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {}
 
 config.host = process.env.HOST || "https://northwind.documents.azure.com:443/";
 config.authKey = process.env.AUTH_KEY || "90xMbUUvgElya0EzuoxXGcgcVFMoEq80RSkLkAC5gd2xJy4RYwYt5FnqI0VXSATzY766wdV0F7EjI1WE8R1vyQ==";
-config.databaseId = "ToDoList";
+config.databaseId = "northwind";
 config.collectionId = "Territories";
 
 module.exports = config;
